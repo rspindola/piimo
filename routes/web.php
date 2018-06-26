5bc61bd954cd18b7c07ef290435ef97cc6b742df
criacao <didiraja@hotmail.com>
2018-06-26 17:56:31 -0300

    home

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/home','HomeController@index');
     Route::resource('/empreendimentos', 'DevelopmentController');
     Route::resource('/leads', 'LeadController');
     Route::resource('/images', 'ImageController');

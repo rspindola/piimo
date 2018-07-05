3d5be560206a3d8de0a254b528a9761ee6e07a01
criacao <didiraja@hotmail.com>
2018-07-05 12:09:18 -0300

    exportar tabela e newsletter

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -55,10 +55,25 @@ $(document).ready(function() {
         },
         dom: 'Bfrtip',
         buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0, ':visible' ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 5 ]
+                }
+            },
+            'colvis'
         ]
     } );
 } );
@@ -71,4 +86,5 @@ $(document).ready(function() {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
 @stop
